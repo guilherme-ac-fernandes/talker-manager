16fa57f17b0675123248cd7887da2bdafcdd16b1
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-08-02 17:09:20 -0300

    feat: implementa endpoint '/talker' (requisito 1)

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs/promises');
 
 const app = express();
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
+const TALKER_FILE = './talker.json';
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
 
-
+app.get('/talker', async (_req, res) => {
+  const talkers = await fs.readFile(TALKER_FILE, 'utf-8')
+    .then((content) => JSON.parse(content));
+  if (talkers.length === 0) {
+    return res.status(200).json([]);
+  }
+  return res.status(200).json(talkers);
+});
 
 app.listen(PORT, () => {
   console.log('Online');
